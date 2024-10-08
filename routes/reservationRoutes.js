785f5476f414db4bfeb7fea8a8aec1b347fd9d88
Indunil Pathirana <indunil.15@itfac.mrt.ac.lk>
2024-10-08 19:13:57 +0530

    backend connected

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,20 +1,28 @@
-const express = require ("express");
+const express = require("express");
 const router = express.Router();
-const {check} = require('express-validator');
+const { check } = require("express-validator");
 
-const reservationControllers=require('../controllers/reservations-controllers.js')
+const reservationControllers = require("../controllers/reservations-controllers.js");
 
-router.get('/',reservationControllers.getReservations);
+router.get("/", reservationControllers.getReservations);
 
-router.get('/:pid',reservationControllers.getReservationsById);
+router.get("/:pid", reservationControllers.getReservationsById);
 
-router.get('/user/:uID',reservationControllers.getReservationsByUserId);
+router.get("/user/:uID", reservationControllers.getReservationsByUserId);
 
-//Check () is another express middleware used for validating 
-router.post('/',[check('name').not().isEmpty(),check('email').not().isEmpty()], reservationControllers.createReservations);
+//Check () is another express middleware used for validating
+router.post(
+  "/makereservation",
+  //   [check("name").not().isEmpty(), check("email").not().isEmpty()],
+  reservationControllers.createReservations
+);
 
-router.patch('/:pid',[check('name').not().isEmpty()],reservationControllers.updateReservations);
+router.patch(
+  "/:pid",
+  [check("name").not().isEmpty()],
+  reservationControllers.updateReservations
+);
 
-router.delete('/:pid', reservationControllers.deleteReservations);
+router.delete("/:pid", reservationControllers.deleteReservations);
 
-module.exports=router;
+module.exports = router;
